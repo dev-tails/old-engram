728c2b62bc2e9803c4b19d614c2c031cc2997fa0
Adam Berg <adam@xyzdigital.com>
2020-10-17 12:45:49 -0700

    Add mongo connection

--- be/index.js
+++ be/index.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const url = process.env.DB_URL;
+const dbName = process.env.DB_NAME;
+
+run();
+
+async function run() {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  console.log(`Connected to ${dbName}`);
+
+  const app = express();
+
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+
+  app.listen(3000);
+}
