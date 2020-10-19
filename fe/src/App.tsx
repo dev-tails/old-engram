698fc3e560a9664b1a70a0e4d0f7be71973144e7
Adam Berg <adam@xyzdigital.com>
2020-10-18 19:13:32 -0700

    Send post request with body on save click

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, {
+  ChangeEvent,
+  FormEvent,
+  FormEventHandler,
+  useState,
+} from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   const date = new Date();
-  
+  const [body, setBody] = useState("");
+
+  const handleSaveClicked = () => {
+    axios.post("", { body });
+  };
+
+  const handleBodyChanged = (event: FormEvent<HTMLElement>) => {
+    setBody(event.currentTarget.innerText);
+  };
+
   return (
     <div className="App">
       <div id="date">{date.toLocaleDateString()}</div>
-      <div id="body" contentEditable="true"></div>
-      <div id="save">Save</div>
+      <div id="body" contentEditable="true" onInput={handleBodyChanged}></div>
+      <div id="save" onClick={handleSaveClicked}>
+        Save
+      </div>
     </div>
   );
 }
