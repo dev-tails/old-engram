3b4f44a27db9af0c9e0d491febcc8a25102ddc41
Adam Berg <adam@xyzdigital.com>
2020-10-18 18:59:06 -0700

    Add POST route to modify current day's note

--- be/index.js
+++ be/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const dotenv = require("dotenv");
 const moment = require("moment");
+const bodyParser = require("body-parser");
+
 dotenv.config();
 
 const url = process.env.DB_URL;
@@ -15,12 +17,28 @@ async function run() {
   console.log(`Connected to ${dbName}`);
 
   const app = express();
+  app.use(bodyParser);
 
   app.get("/", async function (req, res) {
-    const currentDateString = moment().format("YYYY-MM-DD")
+    const currentDateString = moment().format("YYYY-MM-DD");
 
-    const note = await db.collection('notes').findOne({date: currentDateString})
-    return res.json(note)
+    const note = await db
+      .collection("notes")
+      .findOne({ date: currentDateString });
+    return res.json(note);
+  });
+
+  app.post("/", async function (req, res) {
+    const currentDateString = moment().format("YYYY-MM-DD");
+
+    const note = await db
+      .collection("notes")
+      .updateOne(
+        { date: currentDateString },
+        { body: req.body.body },
+        { upsert: true }
+      );
+    return res.json(note);
   });
 
   app.listen(3000);
