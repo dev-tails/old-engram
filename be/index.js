a7962a6fabdfc9a01e6d49c86a1d724e37ff92db
Adam Berg <adam@xyzdigital.com>
2020-11-12 19:58:29 -0800

    Add access control headers

--- be/index.js
+++ be/index.js
@@ -10,6 +10,7 @@ const bcrypt = require("bcrypt");
 
 dotenv.config();
 
+const origin = process.env.ORIGIN;
 const url = process.env.DB_URL;
 const port = Number(process.env.PORT) || 4000;
 const jwtSecret = process.env.JWT_SECRET;
@@ -25,6 +26,14 @@ async function run() {
   console.log(`Connected`);
 
   const app = express();
+
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
   app.use(bodyParser.json());
   app.use(cookieParser());
 
