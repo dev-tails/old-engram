45cb031a4432bd45bd58d4c3e3c29378da89f897
Adam Berg <adam@xyzdigital.com>
2020-11-14 11:14:53 -0800

    Add simple header with "All"

---
+++ fe/src/components/header/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.scss";
+
+type HeaderProps = {};
+
+export const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <div className="header">
+      <div className="left-button">
+        {/* <button>&lt;</button> */}
+      </div>
+      <div className="title">All</div>
+      <div className="right-button">
+        {/* <button>...</button> */}
+      </div>
+    </div>
+  );
+};
