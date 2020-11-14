2bcb81329f883620396fcf6a3d1a56f2d81df113
Adam Berg <adam@xyzdigital.com>
2020-11-14 11:14:53 -0800

    Add simple header with "All"

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./components/header/Header";
 import Routes from "./routes/Routes";
 
 function App() {
   return (
-    <Routes></Routes>
-  )
+    <div>
+      <Header />
+      <Routes></Routes>
+    </div>
+  );
 }
 
 export default App;
