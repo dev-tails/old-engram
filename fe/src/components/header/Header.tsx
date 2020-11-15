42f135634e29c39b9d76e4e7c2e941eb1ccbeb5f
Adam Berg <adam@xyzdigital.com>
2020-11-15 08:28:56 -0800

    Different title for login and notes pages

--- fe/src/components/header/Header.tsx
+++ fe/src/components/header/Header.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useLocation } from "react-router";
 import "./Header.scss";
 
 type HeaderProps = {};
 
 export const Header: React.FC<HeaderProps> = (props) => {
+  const location = useLocation();
+
+  let title = "BrainStorm";
+  if (location.pathname === "/notes") {
+    title = "All";
+  }
+
   return (
     <div className="header-wrapper">
       <div className="header">
         <div className="left-button">{/* <button>&lt;</button> */}</div>
-        <div className="title">All</div>
+        <div className="title">{title}</div>
         <div className="right-button">{/* <button>...</button> */}</div>
       </div>
     </div>
