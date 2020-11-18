624fdbc04234f4c0a062d300ce3e7bf01efbe3d8
Adam Berg <adam@dubsado.com>
2020-11-18 08:48:29 -0800

    Fetch items by widget

--- fe/src/components/widgets/WidgetsApi.ts
+++ fe/src/components/widgets/WidgetsApi.ts
@@ -1,24 +1,17 @@
+import axios from "axios";
 import { Note } from "../notes/NotesApi";
 
 export type WidgetData = {
-  _id: string;
+  widget: {
+    _id: string;
+    name: string;
+  };
   items: Note[];
 };
 
 export async function getWidget(id: string): Promise<WidgetData> {
-  return {
-    _id: "1",
-    items: [
-      {
-        _id: "5fb027d8a0c0ebd283add8ae",
-        body: "This is item 1",
-      },
-      {
-        _id: "2",
-        body: "This is item 2",
-      },
-    ],
-  };
+  const res = await axios.get(`/api/widgets/${id}`);
+  return res.data;
 }
 
 export async function submitWidgetNote(widgetId: string, note: string) {}
