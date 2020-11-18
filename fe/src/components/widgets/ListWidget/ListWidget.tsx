f05e675f647664a31cbfdb2684282fe603d6088a
Adam Berg <adam@xyzdigital.com>
2020-11-17 20:29:53 -0800

    Add updateNote when checked

--- fe/src/components/widgets/ListWidget/ListWidget.tsx
+++ fe/src/components/widgets/ListWidget/ListWidget.tsx
@@ -2,18 +2,30 @@ import React from "react";
 import { Note } from "../../notes/NotesApi";
 import "./ListWidget.scss";
 
-type ListWidgetProps = {
+export type ListWidgetProps = {
   items: Note[];
+  onItemChanged: (item: Partial<Note>) => void;
 };
 
 export const ListWidget: React.FC<ListWidgetProps> = (props) => {
+  const handleItemChecked = (item: Note) => {
+    props.onItemChanged({
+      _id: item._id,
+      checked: !item.checked,
+    });
+  };
+
   return (
     <div className="list-widget">
       {props.items.map((item) => {
         return (
           <div key={item._id} className="list-item">
             <div className="checkbox">
-              <input type="checkbox" checked={item.checked} />
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={handleItemChecked.bind(this, item)}
+              />
             </div>
             <div className="item-body">{item.body}</div>
           </div>
