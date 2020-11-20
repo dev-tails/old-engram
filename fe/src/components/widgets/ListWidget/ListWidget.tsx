22e05579116b27298e82b9b3f11c4b0c9b7660f3
Adam Berg <adam@xyzdigital.com>
2020-11-19 19:49:13 -0800

    Allow list view without checkboxes

--- fe/src/components/widgets/ListWidget/ListWidget.tsx
+++ fe/src/components/widgets/ListWidget/ListWidget.tsx
@@ -1,17 +1,38 @@
-import React from "react";
-import { CheckboxList } from "../../CheckboxList/CheckboxList";
+import React, { useEffect, useRef } from "react";
 import { Note } from "../../notes/NotesApi";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 import "./ListWidget.scss";
 
 export type ListWidgetProps = {
   items: Note[];
   onItemChanged: (item: Partial<Note>) => void;
   onItemDeleted: (itemId?: string) => void;
+  checkboxes?: boolean;
 };
 
-export const ListWidget: React.FC<ListWidgetProps> = (props) => {
-  const handleItemChecked = (item: Note) => {
-    props.onItemChanged({
+export const ListWidget: React.FC<ListWidgetProps> = ({
+  items,
+  onItemChanged,
+  onItemDeleted,
+  checkboxes,
+}) => {
+  const lastItemRef = useRef<any>();
+  useEffect(() => {
+    if (!lastItemRef || !lastItemRef.current) {
+      return;
+    }
+    lastItemRef.current?.scrollIntoView({ behaviour: "auto" });
+  }, [items]);
+
+  const handleToggle = (item: Note) => () => {
+    onItemChanged({
       _id: item._id,
       checked: !item.checked,
     });
@@ -19,11 +40,46 @@ export const ListWidget: React.FC<ListWidgetProps> = (props) => {
 
   return (
     <div className="list-widget">
-      <CheckboxList
-        onChecked={handleItemChecked}
-        onDelete={props.onItemDeleted}
-        items={props.items}
-      />
+      <List>
+        {items.map((item, index) => {
+          const labelId = `checkbox-list-label-${item._id}`;
+
+          return (
+            <ListItem
+              key={item._id}
+              role={undefined}
+              dense
+              button
+              ref={index === items.length - 1 ? lastItemRef : null}
+            >
+              {checkboxes && (
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={item.checked}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                    onClick={handleToggle(item)}
+                  />
+                </ListItemIcon>
+              )}
+              <ListItemText id={labelId} primary={item.body} />
+              <ListItemSecondaryAction>
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => {
+                    onItemDeleted(item._id);
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 };
