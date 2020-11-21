a47110fb285d97a4abfd32e6cb6884ae4b5efaaa
Adam Berg <adam@dubsado.com>
2020-11-20 19:33:46 -0800

    Remove Material UI List

--- fe/src/components/widgets/ListWidget/ListWidget.tsx
+++ fe/src/components/widgets/ListWidget/ListWidget.tsx
@@ -1,12 +1,4 @@
 import React, { useEffect, useRef } from "react";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import Checkbox from "@material-ui/core/Checkbox";
 import { Note } from "../../notes/NotesApi";
 import { dateFromObjectId } from "../../../utils/ObjectId";
 import "./ListWidget.scss";
@@ -32,67 +24,36 @@ export const ListWidget: React.FC<ListWidgetProps> = ({
     lastItemRef.current?.scrollIntoView({ behaviour: "auto" });
   }, [items]);
 
-  const handleToggle = (item: Note) => () => {
-    onItemChanged({
-      _id: item._id,
-      checked: !item.checked,
-    });
-  };
+  // const handleToggle = (item: Note) => () => {
+  //   onItemChanged({
+  //     _id: item._id,
+  //     checked: !item.checked,
+  //   });
+  // };
+
+  const handleItemClicked = (item: Note, event: any) => {};
 
   return (
     <div className="list-widget">
-      <List>
-        {items.map((item, index) => {
-          const labelId = `checkbox-list-label-${item._id}`;
-          const itemDate = dateFromObjectId(item._id);
-          const itemDateString = `${itemDate?.toLocaleDateString()} ${itemDate?.toLocaleTimeString()}`;
+      {items.map((item) => {
+        if (!item.body) {
+          return null;
+        }
 
-          return (
-            <ListItem
-              key={item._id}
-              role={undefined}
-              dense
-              button
-              ref={index === items.length - 1 ? lastItemRef : null}
-            >
-              {checkboxes && (
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={item.checked}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ "aria-labelledby": labelId }}
-                    onClick={handleToggle(item)}
-                  />
-                </ListItemIcon>
-              )}
-              <ListItemText
-                id={labelId}
-                primary={item.body}
-                secondary={itemDateString}
-                primaryTypographyProps={{
-                  style: {
-                    whiteSpace: "break-spaces",
-                    wordBreak: "break-word",
-                  },
-                }}
-              />
-              <ListItemSecondaryAction>
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={() => {
-                    onItemDeleted(item._id);
-                  }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
-      </List>
+        const itemDate = dateFromObjectId(item._id);
+        const itemDateString = `${itemDate?.toLocaleDateString()} ${itemDate?.toLocaleTimeString()}`;
+
+        return (
+          <div
+            key={item._id}
+            className="list-item"
+            onClick={handleItemClicked.bind(this, item)}
+          >
+            <div className="list-item-text">{item.body}</div>
+            <div className="list-item-secondary">{itemDateString}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
