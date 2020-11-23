e1c3b71d56e7492a715879450b141960cd6291ba
Adam Berg <adam@dubsado.com>
2020-11-22 21:42:30 -0800

    Add Zapier Help page

--- fe/src/components/HelpPage/ZapierHelpPage/ZapierHelpPage.tsx
+++ fe/src/components/HelpPage/ZapierHelpPage/ZapierHelpPage.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { createKey, deleteKey } from "../../../ZapierApi";
 import { Header } from "../../header/Header";
+import ApiKeyImage from "./api-key.png";
+import ZapierSignInImage from "./zapier-sign-in-to-engram.png";
 import "./ZapierHelpPage.scss";
 
 type ZapierHelpPageProps = {};
@@ -40,22 +42,57 @@ export const ZapierHelpPage: React.FC<ZapierHelpPageProps> = (props) => {
       <Header title="Zapier Help" />
       <div className="container" style={{ marginTop: 64 }}>
         <h2>How to Set up Zapier</h2>
-        <h3>Create an API Key</h3>
-        {keys &&
-          keys.map((key, index) => {
-            return (
-              <div>
-                <input value={"*".repeat(64)} readOnly={true} disabled={true} />
-                <button
-                  onClick={handleDeleteClicked.bind(this, key._id, index)}
-                >
-                  Delete
-                </button>
-              </div>
-            );
-          })}
-        <input value={key} />
-        <button onClick={handleCreateClicked}>Create</button>
+        <h3>Create an API key</h3>
+        <div className="keys">
+          {keys &&
+            keys.map((key, index) => {
+              return (
+                <tr className="key">
+                  <td>
+                    <input
+                      value={"*".repeat(64)}
+                      readOnly={true}
+                      disabled={true}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={handleDeleteClicked.bind(this, key._id, index)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          <tr className="key">
+            <td>
+              <input value={key} readOnly disabled />
+            </td>
+            <td>
+              <button onClick={handleCreateClicked}>Create</button>
+            </td>
+          </tr>
+        </div>
+        <ol>
+          <li>Press Create button above</li>
+          <li>
+            Copy the API key (it will no longer be visible after refreshing the
+            page)
+          </li>
+          <li>
+            Create a new Action or Trigger in&nbsp;
+            <a href="https://zapier.com/app/zaps">Zapier</a>
+          </li>
+          <li>
+            Click "Choose Account"
+            <img src={ZapierSignInImage} />
+          </li>
+          <li>
+            Paste API key and click "Yes, continue"
+            <img src={ApiKeyImage} />
+          </li>
+        </ol>
       </div>
     </div>
   );
