7c68c226e8eb95d02aaafdba591fe41f2b4b45cb
Adam Berg <adam@dubsado.com>
2020-11-24 19:58:27 -0800

    Start to widget list menu

--- fe/src/components/header/Header.tsx
+++ fe/src/components/header/Header.tsx
@@ -1,6 +1,15 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  List,
+  ListItem,
+  ListItemText,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React from "react";
+import React, { useState } from "react";
+import "./Header.scss";
 
 type HeaderProps = {
   title: string;
@@ -15,15 +24,34 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const Header: React.FC<HeaderProps> = ({ title }) => {
+  const [drawerOpen, setDrawerOpen] = useState(true);
   const classes = useStyles();
 
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          {title}
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <React.Fragment>
+        <SwipeableDrawer
+          anchor={"left"}
+          open={drawerOpen}
+          onClose={setDrawerOpen.bind(this, false)}
+          onOpen={setDrawerOpen.bind(this, true)}
+        >
+          <div className="drawer-contents">
+            <List>
+              <ListItem button>
+                <ListItemText primary="Widget Name" />
+              </ListItem>
+            </List>
+          </div>
+        </SwipeableDrawer>
+      </React.Fragment>
+    </>
   );
 };
