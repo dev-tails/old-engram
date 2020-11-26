2c6a710ffa9afd473a06aefbf052d97ef8546aa1
Adam Berg <adam@xyzdigital.com>
2020-11-26 19:03:54 +0000

    Allow virtually submitting notes by saying "Engram"

--- fe/src/components/textbox/TextBox.tsx
+++ fe/src/components/textbox/TextBox.tsx
@@ -5,49 +5,89 @@ type TextBoxProps = {
   onSubmit: (body: string) => void;
 };
 
+function endsWithSubmitPhrase(textToCheck: string): string {
+  const submitPhrases = [
+    "in the ground",
+    "and cram",
+    "in grand",
+    "an gram",
+    "engram",
+    "end gram",
+    "and graham",
+    "in graham",
+  ];
+  for (const submitPhrase of submitPhrases) {
+    const endSubstringToCheck = textToCheck.substr(
+      textToCheck.length - submitPhrase.length
+    );
+    const submitPhraseDetected = endSubstringToCheck
+      .toLowerCase()
+      .includes(submitPhrase);
+    if (submitPhraseDetected) {
+      return submitPhrase;
+    }
+  }
+  return "";
+}
+
 export default function TextBox(props: TextBoxProps) {
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [
+    newLineTimeoutId,
+    setNewLinePhraseTimeoutId,
+  ] = useState<NodeJS.Timeout | null>(null);
   const [
     submitPhraseTimeoutId,
     setSubmitPhraseTimeoutId,
   ] = useState<NodeJS.Timeout | null>(null);
   const [virtualMode, setVirtualMode] = useState(false);
   const [note, setNote] = useState("");
+  const [virtuallySubmittedNotes, setVirtuallySubmittedNotes] = useState<
+    string[]
+  >([]);
 
   const handleNoteChanged: React.TextareaHTMLAttributes<HTMLTextAreaElement>["onChange"] = (
     event
   ) => {
     let newNote = event?.target?.value;
+    let virtualNote = newNote;
 
-    if (submitPhraseTimeoutId) {
-      clearTimeout(submitPhraseTimeoutId);
-      setSubmitPhraseTimeoutId(null);
-    }
-
-    const submitPhrases = ["engram", "end gram", "and graham"];
-    for (const submitPhrase of submitPhrases) {
-      const endSubstringToCheck = newNote.substr(
-        newNote.length - submitPhrase.length
-      );
-      const submitPhraseDetected = endSubstringToCheck
-        .toLowerCase()
-        .includes(submitPhrase);
-      if (submitPhraseDetected) {
-        setVirtualMode(true);
-
-        const bodyWithoutSubmitPhrase = newNote
-          .substr(0, newNote.length - submitPhrase.length)
-          .trimRight();
-
-        const submitFunc = handleSubmit.bind(null, bodyWithoutSubmitPhrase);
-
-        const submitPhraseDelayMilliseconds = 1000;
-        setSubmitPhraseTimeoutId(
-          setTimeout(submitFunc, submitPhraseDelayMilliseconds)
-        );
+    if (virtualMode) {
+      for (const virtuallySubmittedNote of virtuallySubmittedNotes) {
+        virtualNote = virtualNote.replace(virtuallySubmittedNote, "");
       }
+
+      console.log(`Virtual Note: ${virtualNote}`);
     }
 
+    if (newLineTimeoutId) {
+      console.log("Cleared new line timeout");
+
+      clearTimeout(newLineTimeoutId);
+      setNewLinePhraseTimeoutId(null);
+    }
+
+    const submitPhraseDetected = endsWithSubmitPhrase(virtualNote);
+    if (submitPhraseDetected) {
+      setVirtualMode(true);
+
+      console.log(`Adding virtual note: ${virtualNote}`);
+      setVirtuallySubmittedNotes([...virtuallySubmittedNotes, virtualNote]);
+
+      const bodyWithoutSubmitPhrase = virtualNote
+        .substr(0, virtualNote.length - submitPhraseDetected.length)
+        .trimRight();
+
+      const submitPhraseDelayMilliseconds = 200;
+      console.log("setNewLinePhraseTimeoutId");
+      setNewLinePhraseTimeoutId(
+        setTimeout(() => {
+          handleSubmit(bodyWithoutSubmitPhrase, true);
+        }, submitPhraseDelayMilliseconds)
+      );
+    }
+
+    console.log(`Setting note to ${newNote}`);
     setNote(newNote);
   };
 
@@ -60,15 +100,21 @@ export default function TextBox(props: TextBoxProps) {
     }
   };
 
-  const handleSubmit = (body: string) => {
+  const handleSubmit = (body: string, virtualMode: boolean = false) => {
     props.onSubmit(body);
-    setNote("");
 
     if (virtualMode) {
+      console.log("submitted virtually");
+    } else {
+      console.log("submitted not virtually");
+      setNote("");
+    }
+  };
+
+  const handleBlur = () => {
+    if (virtualMode) {
       setVirtualMode(false);
-      if (textAreaRef.current) {
-        textAreaRef.current.blur();
-      }
+      setNote("");
     }
   };
 
@@ -79,6 +125,7 @@ export default function TextBox(props: TextBoxProps) {
         value={note}
         onKeyDown={handleKeyDown}
         onChange={handleNoteChanged}
+        onBlur={handleBlur}
         rows={1}
         ref={textAreaRef}
       />
