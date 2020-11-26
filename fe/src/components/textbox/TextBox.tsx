25595ff5e990160f3225927923a827430cf61089
Adam Berg <adam@xyzdigital.com>
2020-11-26 17:35:23 +0000

    Start to submitting notes verbally

--- fe/src/components/textbox/TextBox.tsx
+++ fe/src/components/textbox/TextBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./TextBox.scss";
 
 type TextBoxProps = {
@@ -6,10 +6,49 @@ type TextBoxProps = {
 };
 
 export default function TextBox(props: TextBoxProps) {
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [
+    submitPhraseTimeoutId,
+    setSubmitPhraseTimeoutId,
+  ] = useState<NodeJS.Timeout | null>(null);
+  const [virtualMode, setVirtualMode] = useState(false);
   const [note, setNote] = useState("");
 
-  const handleNoteChanged = (event: any) => {
-    setNote(event?.target?.value);
+  const handleNoteChanged: React.TextareaHTMLAttributes<HTMLTextAreaElement>["onChange"] = (
+    event
+  ) => {
+    let newNote = event?.target?.value;
+
+    if (submitPhraseTimeoutId) {
+      clearTimeout(submitPhraseTimeoutId);
+      setSubmitPhraseTimeoutId(null);
+    }
+
+    const submitPhrases = ["engram", "end gram", "and graham"];
+    for (const submitPhrase of submitPhrases) {
+      const endSubstringToCheck = newNote.substr(
+        newNote.length - submitPhrase.length
+      );
+      const submitPhraseDetected = endSubstringToCheck
+        .toLowerCase()
+        .includes(submitPhrase);
+      if (submitPhraseDetected) {
+        setVirtualMode(true);
+
+        const bodyWithoutSubmitPhrase = newNote
+          .substr(0, newNote.length - submitPhrase.length)
+          .trimRight();
+
+        const submitFunc = handleSubmit.bind(null, bodyWithoutSubmitPhrase);
+
+        const submitPhraseDelayMilliseconds = 1000;
+        setSubmitPhraseTimeoutId(
+          setTimeout(submitFunc, submitPhraseDelayMilliseconds)
+        );
+      }
+    }
+
+    setNote(newNote);
   };
 
   const handleKeyDown: React.DOMAttributes<HTMLTextAreaElement>["onKeyDown"] = (
@@ -17,8 +56,19 @@ export default function TextBox(props: TextBoxProps) {
   ) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
-      props.onSubmit(note);
-      setNote("");
+      handleSubmit(note);
+    }
+  };
+
+  const handleSubmit = (body: string) => {
+    props.onSubmit(body);
+    setNote("");
+
+    if (virtualMode) {
+      setVirtualMode(false);
+      if (textAreaRef.current) {
+        textAreaRef.current.blur();
+      }
     }
   };
 
@@ -30,6 +80,7 @@ export default function TextBox(props: TextBoxProps) {
         onKeyDown={handleKeyDown}
         onChange={handleNoteChanged}
         rows={1}
+        ref={textAreaRef}
       />
     </div>
   );
