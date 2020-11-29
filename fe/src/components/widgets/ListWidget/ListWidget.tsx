afa6f4624471420e9f95a895ab83b66ecb9c4c0a
Adam Berg <adam@xyzdigital.com>
2020-11-29 03:43:52 +0000

    Remove react-virtualized

--- fe/src/components/widgets/ListWidget/ListWidget.tsx
+++ fe/src/components/widgets/ListWidget/ListWidget.tsx
@@ -1,13 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Note } from "../../notes/NotesApi";
 import "./ListWidget.scss";
 import { ListWidgetItem } from "./ListWidgetItem/ListWidgetItem";
-import {
-  AutoSizer,
-  CellMeasurer,
-  CellMeasurerCache,
-  List,
-} from "react-virtualized";
 
 export type ListWidgetProps = {
   items: Note[];
@@ -17,76 +11,14 @@ export type ListWidgetProps = {
   hideDelete?: boolean;
 };
 
-type RowRendererParams = {
-  parent: any;
-  key: string;
-  index: number;
-  isScrolling: boolean;
-  isVisible: boolean;
-  style: any;
-};
-
 export const ListWidget: React.FC<ListWidgetProps> = (props) => {
-  const listRef = useRef<List | null>(null);
-  const { items } = props;
-
-  useEffect(() => {
-    if (listRef.current) {
-      let list = listRef.current;
-      setTimeout(() => {
-        list.scrollToRow(props.items.length - 1);
-      }, 200);
-    }
-  }, [props.items]);
-
-  const cellMeasurerCache = new CellMeasurerCache({
-    fixedWidth: true,
-  });
-
-  let mostRecentWidth = 0;
-
-  const rowRenderer = (params: RowRendererParams) => {
-    const { key, index } = params;
-
-    const item = props.items[index];
-
-    return (
-      <CellMeasurer
-        cache={cellMeasurerCache}
-        columnIndex={0}
-        key={key}
-        parent={params.parent}
-        rowIndex={params.index}
-        width={mostRecentWidth}
-      >
-        <div style={params.style}>
-          <ListWidgetItem index={index} item={item} {...props} />
-        </div>
-      </CellMeasurer>
-    );
-  };
-
   return (
     <div className="list-widget">
-      <div style={{ flex: "1 1 auto" }}>
-        <AutoSizer>
-          {({ height, width }: { height: number; width: number }) => {
-            mostRecentWidth = width;
-
-            return (
-              <List
-                ref={listRef}
-                deferredMeasurementCache={cellMeasurerCache}
-                width={width}
-                height={height}
-                rowCount={items.length}
-                rowHeight={cellMeasurerCache.rowHeight}
-                rowRenderer={rowRenderer}
-              />
-            );
-          }}
-        </AutoSizer>
-      </div>
+      {props.items.map((item, index) => {
+        return (
+          <ListWidgetItem key={item._id} index={index} item={item} {...props} />
+        );
+      })}
     </div>
   );
 };
