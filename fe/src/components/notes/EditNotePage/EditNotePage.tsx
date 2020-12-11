c3767ffc339315fb2a01a9af59b146b7182e91ab
Adam Berg <adam@xyzdigital.com>
2020-12-11 01:45:02 +0000

    Sort children using prev property

--- fe/src/components/notes/EditNotePage/EditNotePage.tsx
+++ fe/src/components/notes/EditNotePage/EditNotePage.tsx
@@ -12,36 +12,72 @@ type EditNotePageParams = {
 export const EditNotePage: React.FC<EditNotePageProps> = (props) => {
   const params = useParams<EditNotePageParams>();
 
-  useEffect(() => {});
+  const notes = [
+    {
+      _id: "1",
+      body: "Title",
+    },
+    {
+      _id: "3",
+      body: "Title Child 2",
+      parent: "1",
+      prev: "2",
+    },
+    {
+      _id: "2",
+      body: "Title Child 1",
+      parent: "1",
+      next: "3",
+    },
+    {
+      _id: "4",
+      body: "Child 2 - Child 2",
+      parent: "3",
+      prev: "5",
+    },
+    {
+      _id: "5",
+      body: "Child 2 - Child 1",
+      parent: "3",
+    },
+  ];
 
-  const note = {
-    body: "Title",
-    children: [
-      {
-        body: "Title Child 1",
-        children: [
-          {
-            body: "Child 1 Child 1",
-          },
-          {
-            body: "Child 1 Child 2",
-          },
-        ],
-      },
-      {
-        body: "Title Child 2",
-        children: [
-          {
-            body: "Child 2 Child 1",
-          },
-        ],
-      },
-    ],
-  };
+  const note = notes.find((note) => note._id === params.id);
+  if (!note) {
+    return null;
+  }
+
+  const children = notes.filter((note) => note.parent === params.id);
+  const sortedChildren = [];
+  let prev = null;
+  do {
+    let found = false;
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+
+      if (!prev && !child.prev) {
+        found = true;
+      } else if (prev === child.prev) {
+        found = true;
+      }
+
+      if (found) {
+        prev = child._id;
+        sortedChildren.push(child);
+        break;
+      }
+    }
+
+    if (!found) {
+      prev = null;
+    }
+  } while (prev !== null);
+
+  const populatedNote = { ...note, children: sortedChildren };
 
   return (
     <div className="edit-note-page">
-      <CollapsibleNoteItem note={note} onSave={() => {}} />
+      <CollapsibleNoteItem note={populatedNote} onSave={() => {}} />
     </div>
   );
 };
