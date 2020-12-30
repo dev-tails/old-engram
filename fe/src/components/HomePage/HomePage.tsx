1c0badf2cca5292d751f34cf41039213f1ccf778
Adam Berg <adam@xyzdigital.com>
2020-12-30 08:32:59 -0800

    Start to bottom navigation

--- fe/src/components/HomePage/HomePage.tsx
+++ fe/src/components/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import './HomePage.scss';
 
-import React from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import React, { useState } from 'react';
 
 import { AgendaViewPage } from '../AgendaViewPage/AgendaViewPage';
 import NotesPage from '../notes/NotesPage';
@@ -8,16 +9,33 @@ import NotesPage from '../notes/NotesPage';
 type HomePageProps = { date: Date };
 
 export const HomePage: React.FC<HomePageProps> = ({ date }) => {
+  const [bottomNavValue, setBottomNavValue] = useState("note");
+
+  const handleChange = (event: any, newValue: string) => {
+    setBottomNavValue(newValue);
+  };
+
   return (
     <div className="home-page">
-      <div className="events">
+      <div className={`events ${bottomNavValue === "event" ? "visible" : ""}`}>
         <AgendaViewPage date={date} />
       </div>
-      <div className="tasks">
+      <div className={`tasks ${bottomNavValue === "task" ? "visible" : ""}`}>
         <NotesPage date={date} type="task" />
       </div>
-      <div className="notes">
+      <div className={`notes ${bottomNavValue === "note" ? "visible" : ""}`}>
         <NotesPage date={date} type="note" />
+      </div>
+      <div className="bottom-navigation">
+        <BottomNavigation
+          value={bottomNavValue}
+          onChange={handleChange}
+          showLabels
+        >
+          <BottomNavigationAction label="Events" value="event" />
+          <BottomNavigationAction label="Tasks" value="task" />
+          <BottomNavigationAction label="Notes" value="note" />
+        </BottomNavigation>
       </div>
     </div>
   );
