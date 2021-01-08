16328659ca0a8f2a7952158bbdd5389bc57a0e89
Adam Berg <adam@dubsado.com>
2021-01-07 19:07:18 -0800

    Allow submitting quick note via ctrl + enter

---
+++ fe/src/components/AddNoteDialog/AddNoteDialog.tsx
@@ -0,0 +1,75 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField, { TextFieldProps } from '@material-ui/core/TextField';
+import React, { KeyboardEventHandler, useEffect, useState } from 'react';
+
+type AddNoteDialogProps = {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: (body: string) => void;
+};
+
+export const AddNoteDialog: React.FC<AddNoteDialogProps> = ({
+  open,
+  onClose,
+  onSubmit,
+}) => {
+  const [body, setBody] = useState("");
+
+  const handleCancel = () => {
+    onClose();
+    setBody("");
+  };
+
+  const handleSubmit = () => {
+    onSubmit(body);
+    setBody("");
+  };
+
+  const handleKeyPress: KeyboardEventHandler = (event) => {
+    if (event.ctrlKey && event.key === "Enter") {
+      if (open) {
+        event.preventDefault();
+        handleSubmit();
+      }
+    }
+  };
+
+  const handleTextFieldChange: TextFieldProps["onChange"] = (event) => {
+    setBody(event.currentTarget.value);
+  };
+
+  return (
+    <div className="add-note-dialog">
+      <Dialog
+        open={open}
+        fullScreen
+        onClose={handleCancel}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogActions>
+          <Button onClick={handleCancel} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="note"
+            fullWidth
+            multiline
+            value={body}
+            onChange={handleTextFieldChange}
+            onKeyPress={handleKeyPress}
+          />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
--- fe/src/components/FloatingActionButton/FloatingActionButton.tsx
+++ fe/src/components/FloatingActionButton/FloatingActionButton.tsx
@@ -1,14 +1,10 @@
 import './FloatingActionButton.scss';
 
 import { Fab } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import TextField, { TextFieldProps } from '@material-ui/core/TextField';
 import AddIcon from '@material-ui/icons/Add';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { AddNoteDialog } from '../AddNoteDialog/AddNoteDialog';
 import { Note } from '../notes/NotesApi';
 
 type FloatingActionButtonProps = {
@@ -19,11 +15,9 @@ export const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({
   onSubmit,
 }) => {
   const [open, setOpen] = useState(false);
-  const [body, setBody] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (body: string) => {
     onSubmit({ body });
-    setBody("");
     handleClose();
   };
 
@@ -31,38 +25,13 @@ export const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({
     setOpen(false);
   };
 
-  const handleTextFieldChange: TextFieldProps["onChange"] = (event) => {
-    setBody(event.currentTarget.value);
-  };
-
   return (
     <div className="floating-action-button">
-      <Dialog
+      <AddNoteDialog
         open={open}
-        fullScreen
         onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Submit
-          </Button>
-        </DialogActions>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="note"
-            fullWidth
-            multiline
-            value={body}
-            onChange={handleTextFieldChange}
-          />
-        </DialogContent>
-      </Dialog>
+        onSubmit={handleSubmit}
+      />
       <Fab onClick={setOpen.bind(this, true)}>
         <AddIcon />
       </Fab>
