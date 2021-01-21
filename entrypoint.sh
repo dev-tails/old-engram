620735b4ddca062cbff35695510dc8cd84a1e2d9
Adam Berg <adam@xyzdigital.com>
2021-01-20 21:17:16 -0800

    Working dockerfile with API and FE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-echo "Starting"
 yarn --cwd be/ start &
 P1=$!
-echo "Starting 2"
 nginx -g 'daemon off;' & 
 P2=$!
-echo "Starting 3"
 wait $P1 $P2
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -it -p 80:80 --rm engram:latest
