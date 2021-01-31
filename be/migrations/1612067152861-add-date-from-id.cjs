46f05bf5357c443eadd6adba578821daeecd284a
Adam Berg <adam@xyzdigital.com>
2021-01-30 21:04:52 -0800

    Add migration for adding date property

---
+++ be/migrations/1612067152861-add-date-from-id.cjs
@@ -0,0 +1,41 @@
+const dotenv = require("dotenv");
+const mongodb = require("mongodb");
+const moment = require("moment");
+
+dotenv.config();
+
+async function initializeDb() {
+  const url = process.env.DB_URL;
+  const client = await mongodb.MongoClient.connect(url);
+  db = client.db();
+  return db;
+}
+
+function dateFromObjectId(objectId) {
+  if (!objectId) {
+    return null;
+  }
+  return new Date(parseInt(objectId.substring(0, 8), 16) * 1000);
+}
+
+module.exports.up = async function () {
+  const db = await initializeDb();
+  const notesWithoutDates = await db
+    .collection("notes")
+    .find({ date: null }, { projection: { _id: 1 } })
+    .toArray();
+  for (const note of notesWithoutDates) {
+    const date = dateFromObjectId(String(note._id));
+    const dateString = moment(date).format("YYYY-MM-DD");
+    await db
+      .collection("notes")
+      .updateOne({ _id: note._id }, { $set: { date: dateString } });
+  }
+};
+
+module.exports.down = async function () {
+  const db = await initializeDb();
+  await db
+    .collection("notes")
+    .updateMany({ date: { $ne: null } }, { $unset: { date: 1 } });
+};
