35e13a63883e8ebb96140f3bed3b7e97b62f8eba
Adam Berg <adam@xyzdigital.com>
2021-03-04 17:53:43 -0800

    Ability to edit title of page inline

--- fe/src/components/notes/EditNotePage/EditNotePage.tsx
+++ fe/src/components/notes/EditNotePage/EditNotePage.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { CollapsibleNotesList } from "../CollapsibleNotesList/CollapsibleNotesList";
-import { getNote, Note } from "../NotesApi";
-import moment from "moment";
+import { getNote, Note, updateNote } from "../NotesApi";
 import "./EditNotePage.scss";
 
 type EditNotePageProps = {};
@@ -13,22 +11,40 @@ type EditNotePageParams = {
 
 export const EditNotePage: React.FC<EditNotePageProps> = (props) => {
   const params = useParams<EditNotePageParams>();
-  const [notes, setNotes] = useState<Note[]>([]);
-  const [lastUpdate, setLastUpdate] = useState("");
+  const [notes, setNotes] = useState<Note[] | null>(null);
 
   useEffect(() => {
     async function fetchNote() {
       const fetchedNotes = await getNote({ id: params.id });
       setNotes(fetchedNotes);
-      setLastUpdate(moment().format());
     }
     fetchNote();
   }, [params.id]);
 
+  if (!notes) {
+    return null;
+  }
+
+  const topLevelNote = notes[0];
+
+  const handleTitleChanged = (e: React.FocusEvent<HTMLDivElement>) => {
+    updateNote({
+      _id: params.id,
+      body: e.target.innerText,
+    });
+  };
+
   return (
     <div className="edit-note-page">
       <div className="edit-note-page-content">
-        <CollapsibleNotesList key={lastUpdate} notes={notes} />
+        <div
+          className="title"
+          contentEditable={true}
+          onBlur={(e) => handleTitleChanged(e)}
+          suppressContentEditableWarning={true}
+        >
+          {topLevelNote.body}
+        </div>
       </div>
     </div>
   );
