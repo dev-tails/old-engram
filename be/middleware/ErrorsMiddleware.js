25033828371b261f03a3446e3daf7a99770202b4
Adam Berg <adam@xyzdigital.com>
2021-03-20 10:51:31 -0700

    Prevent 401 from clearing token

--- be/middleware/ErrorsMiddleware.js
+++ be/middleware/ErrorsMiddleware.js
@@ -1,6 +1,5 @@
 export function ErrorsMiddleware(err, req, res, next) {
   if (err.name === "UnauthorizedError") {
-    res.clearCookie("token");
     return res.sendStatus(401);
   }
   console.error(err);
