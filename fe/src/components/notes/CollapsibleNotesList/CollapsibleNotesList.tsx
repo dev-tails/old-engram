641b51920c7d04b0322bca3acafe3a47ca942488
Adam Berg <adam@xyzdigital.com>
2021-03-28 11:51:16 -0700

    Fix dropping note on empty note

--- fe/src/components/notes/CollapsibleNotesList/CollapsibleNotesList.tsx
+++ fe/src/components/notes/CollapsibleNotesList/CollapsibleNotesList.tsx
@@ -178,6 +178,7 @@ export const CollapsibleNotesList: React.FC<CollapsibleNotesListProps> = (
         onActivate={handleNoteActivate}
         onDelete={handleDelete}
         onBlur={handleBlur}
+        onDrop={handleDrop}
       />
     </div>
   );
