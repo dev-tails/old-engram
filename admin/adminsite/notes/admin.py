dbcd130465e0e3e2c98959a303edc275b618f892
Adam Berg <adam@xyzdigital.com>
2021-04-25 17:59:49 -0700

    Add User to notes app

--- admin/adminsite/notes/admin.py
+++ admin/adminsite/notes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Note
+from .models import Note, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("_id", "username", "email")
 
 
 @admin.register(Note)
--- admin/adminsite/notes/models.py
+++ admin/adminsite/notes/models.py
@@ -6,13 +6,37 @@ NOTE_TYPE_CHOICES = [
     ("event", "Event"),
     ("task", "Task"),
     ("task_completed", "Completed Task"),
+    ("page", "Page"),
+    ("workspace", "Workspace"),
 ]
+
+
+class User(models.Model):
+    _id = ObjectIdField()
+    username = models.CharField(max_length=60)
+    email = models.EmailField()
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
 
 
 class Note(models.Model):
     _id = ObjectIdField()
+    localId = models.CharField(max_length=37)
+
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+
     type = models.CharField(max_length=15, choices=NOTE_TYPE_CHOICES, default="note")
     body = models.TextField()
+
+    start = models.DateTimeField()
+
+    createdAt = models.DateTimeField()
+    syncedAt = models.DateTimeField()
+    updatedAt = models.DateTimeField()
 
     class Meta:
         db_table = "notes"
