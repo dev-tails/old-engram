1c9fea046da3338c13d587ad3959224a86604cd4
Adam Berg <adam@xyzdigital.com>
2021-04-28 18:25:24 -0700

    Update Zapier Settings page

--- fe/src/components/HelpPage/ZapierHelpPage/ZapierHelpPage.tsx
+++ fe/src/components/HelpPage/ZapierHelpPage/ZapierHelpPage.tsx
@@ -1,11 +1,12 @@
-import './ZapierHelpPage.scss';
+import "./ZapierHelpPage.scss";
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-import { createKey, deleteKey } from '../../../ZapierApi';
-import ApiKeyImage from './api-key.png';
-import ZapierSignInImage from './zapier-sign-in-to-engram.png';
+import { createKey, deleteKey } from "../../../ZapierApi";
+import ApiKeyImage from "./api-key.png";
+import MakeAZap from "./make-a-zap.png";
+import ZapierSignInImage from "./zapier-sign-in-to-engram.png";
 
 type ZapierHelpPageProps = {};
 type Key = { _id: string };
@@ -21,10 +22,8 @@ export const ZapierHelpPage: React.FC<ZapierHelpPageProps> = (props) => {
       setKeys(keys);
     }
 
-    if (!keys) {
-      getKeys();
-    }
-  });
+    getKeys();
+  }, []);
 
   const handleCreateClicked = async () => {
     const newKey = await createKey();
@@ -40,7 +39,7 @@ export const ZapierHelpPage: React.FC<ZapierHelpPageProps> = (props) => {
 
   return (
     <div className="zapier-help-page">
-      <div className="container" style={{ marginTop: 64 }}>
+      <div className="container">
         <h2>How to Set up Zapier</h2>
         <h3>Create an API key</h3>
         <div className="keys">
@@ -48,7 +47,7 @@ export const ZapierHelpPage: React.FC<ZapierHelpPageProps> = (props) => {
             keys.map((key, index) => {
               return (
                 <tr className="key">
-                  <td>
+                  <td width="100%">
                     <input
                       value={"*".repeat(64)}
                       readOnly={true}
@@ -65,16 +64,22 @@ export const ZapierHelpPage: React.FC<ZapierHelpPageProps> = (props) => {
                 </tr>
               );
             })}
-          <tr className="key">
-            <td>
-              <input value={key} readOnly disabled />
-            </td>
-            <td>
-              <button onClick={handleCreateClicked}>Create</button>
-            </td>
-          </tr>
+          {!keys || !keys.length ? (
+            <tr className="key">
+              <td width="100%">
+                <input value={key} readOnly disabled />
+              </td>
+              <td>
+                <button onClick={handleCreateClicked}>Create</button>
+              </td>
+            </tr>
+          ) : null}
         </div>
         <ol>
+          <li>
+            <a href="https://zapier.com/sign-up/">Create a Zapier account</a>{" "}
+            (if you don't already have one)
+          </li>
           <li>Press Create button above</li>
           <li>
             Copy the API key (it will no longer be visible after refreshing the
@@ -82,10 +87,12 @@ export const ZapierHelpPage: React.FC<ZapierHelpPageProps> = (props) => {
           </li>
           <li>
             Create a new Action or Trigger in&nbsp;
-            <a href="https://zapier.com/app/zaps">Zapier</a>
+            <a href="https://zapier.com/app/zaps">Zapier</a> by clicking "Create
+            Zap" or "Make a Zap"
+            <img alt="Zapier Home Page" src={MakeAZap} />
           </li>
           <li>
-            Click "Choose Account"
+            When Choosing an account, click "Sign in to engram"
             <img alt="Zapier Choose Account Page" src={ZapierSignInImage} />
           </li>
           <li>
