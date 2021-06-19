26e5d4cdcb7b03b9b34dc7f8b5ac99480f1ae623
Adam Berg <adam@xyzdigital.com>
2021-06-19 16:33:21 -0700

    Reset body when note is added

--- fe/src/redux/reducers/NoteReducer.ts
+++ fe/src/redux/reducers/NoteReducer.ts
@@ -1,13 +1,19 @@
 import { Note } from '../../components/notes/NotesApi';
 import { SetNoteAction, SetNoteBodyAction, SetNoteTypeAction } from '../actions/NoteActions';
+import { AddNoteAction } from '../actions/NotesActions';
 
 const INITIAL_STATE: Partial<Note> = { body: "" };
 
 const NoteReducer = (
   state = INITIAL_STATE,
-  action: SetNoteAction | SetNoteBodyAction | SetNoteTypeAction
+  action: AddNoteAction | SetNoteAction | SetNoteBodyAction | SetNoteTypeAction
 ) => {
   switch (action.type) {
+    case "ADD_NOTE":
+      return {
+        ...state,
+        body: "",
+      };
     case "SET_NOTE":
       return action.payload;
     case "SET_NOTE_BODY":
