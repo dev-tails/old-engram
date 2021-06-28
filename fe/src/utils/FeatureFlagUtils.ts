8e48813b58661778a085fa91b645b930c9812f7b
Adam Berg <adam@xyzdigital.com>
2021-06-28 16:19:59 -0700

    Fix feature flags to work offline

--- fe/src/utils/FeatureFlagUtils.ts
+++ fe/src/utils/FeatureFlagUtils.ts
@@ -1,4 +1,4 @@
-import Axios from 'axios';
+import Axios from "axios";
 
 type Flag = {
   _id: string;
@@ -9,15 +9,19 @@ type Flag = {
 const flags: { [flag: string]: Flag } = {};
 
 export async function initializeFeatureFlags() {
-  const res = await Axios.get("/api/flags");
-  const flagsArray = res.data.data;
-  for (const flag of flagsArray) {
-    flags[flag.name] = {
-      ...flag,
-    };
+  try {
+    const res = await Axios.get("/api/flags");
+    const flagsArray = res.data.data;
+    for (const flag of flagsArray) {
+      flags[flag.name] = {
+        ...flag,
+      };
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
 export function isFeatureEnabled(flag: string) {
-  return flags[flag].enabled;
+  return flags[flag]?.enabled;
 }
