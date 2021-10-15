1c28dfe0fd47ca7ee45c1d6773f8c89919b10a56
Adam Berg <adam@xyzdigital.com>
2021-10-15 11:57:06 -0700

    Bring in dummy notes

--- fe/src/pages/SearchPage/SearchPage.tsx
+++ fe/src/pages/SearchPage/SearchPage.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem, ListItemText } from '@material-ui/core';
 import React from 'react';
 
 import { BottomNavigation } from '../../components/BottomNavigation/BottomNavigation';
@@ -5,12 +6,52 @@ import { BottomNavigation } from '../../components/BottomNavigation/BottomNaviga
 type SearchPageProps = {};
 
 export const SearchPage: React.FC<SearchPageProps> = (props) => {
+  const notes = [
+    {
+      body: "This is a test",
+      date: "2021-10-01",
+    },
+    {
+      body: "This is a test",
+      date: "2021-10-01",
+    },
+    {
+      body: "This is a test",
+      date: "2021-10-01",
+    },
+    {
+      body: "This is a test",
+      date: "2021-10-01",
+    },
+    {
+      body: "This is a test",
+      date: "2021-10-01",
+    },
+  ];
+
   function handleSubmit() {}
 
   function handleBottomNavChanged() {}
 
   return (
-    <div className="search-page">
+    <div className="search-page page">
+      <div className="page__content">
+        <div className="page__container">
+          <List>
+            {notes.map((note) => {
+              return (
+                <ListItem>
+                  <ListItemText
+                    primary={note.body}
+                    secondary={note.date}
+                  ></ListItemText>
+                </ListItem>
+              );
+            })}
+          </List>
+        </div>
+      </div>
+
       <div className="search-page__footer">
         <BottomNavigation value={"search"} onChange={handleBottomNavChanged} />
       </div>
