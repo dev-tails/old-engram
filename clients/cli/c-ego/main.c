d5cc45b173d5f2d450512de72550decc025f3a97
Adam Berg <adam@xyzdigital.com>
2022-01-27 14:37:55 -0800

    Add basic reading and writing to file

--- clients/cli/c-ego/main.c
+++ clients/cli/c-ego/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_BLOCK_TITLE_SIZE 256
+#define MAX_BLOCK_TITLE_SIZE 128
 #define MAX_BLOCKS 1024
 
 typedef struct block
@@ -10,10 +10,34 @@ typedef struct block
   char title[MAX_BLOCK_TITLE_SIZE];
 } block_t;
 
+unsigned int current_index = 0;
+block_t blocks[MAX_BLOCKS];
+
+int read_from_file() {
+  FILE *fp = fopen("cego.db", "rb");
+  for (int i = 0; i < MAX_BLOCKS; i++) {
+    block_t *current_block = &blocks[i];
+    fread(current_block, sizeof(char), sizeof(block_t), fp);
+    if (strlen(current_block->title) == 0) {
+      current_index = i + 1;
+      break;
+    }
+  }
+  return fclose(fp);
+}
+
+int write_to_file() {
+  FILE *fp = fopen("cego.db", "w");
+  for (int i = 0; i < current_index; i++) {
+    block_t *current_block = &blocks[i];
+    fwrite(current_block, sizeof(char), sizeof(block_t), fp);
+  }
+  return fclose(fp);
+}
+
 int main()
 {
-  block_t blocks[MAX_BLOCKS];
-  unsigned int current_index = 0;
+  read_from_file();
 
   while (current_index < MAX_BLOCKS)
   {
@@ -28,6 +52,8 @@ int main()
     }
     current_index++;
   }
+
+  write_to_file();
 
   return 0;
 }
