bd9a5ceb799dee09233d293887be951e32935673
Adam Berg <adam@xyzdigital.com>
2022-02-16 17:24:20 -0800

    Add simple login

--- chat/be/server.js
+++ chat/be/server.js
@@ -1,47 +1,78 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongodb = require("mongodb");
-const http = require('http');
+const http = require("http");
+const cookieParser = require("cookie-parser");
 const { Server } = require("socket.io");
 
 dotenv.config();
 
 async function run() {
-    const client = await mongodb.MongoClient.connect(process.env.DB_URL);
-    const db = client.db();
-    const Message = db.collection("messages");
+  const userDatabaseClient = await mongodb.MongoClient.connect(
+    process.env.USER_DB_URL
+  );
+  const userDb = userDatabaseClient.db();
+  const User = userDb.collection("users");
 
-    const app = express();
-    const server = http.createServer(app);
-    const io = new Server(server);
+  const client = await mongodb.MongoClient.connect(process.env.DB_URL);
+  const db = client.db();
+  const Message = db.collection("messages");
+  const Room = db.collection("rooms");
 
-    app.use(express.json());
+  const app = express();
+  const server = http.createServer(app);
+  const io = new Server(server);
 
-    app.use(express.static("../fe/public"));
+  app.use(cookieParser());
+  app.use(express.json());
 
-    app.get("/api/rooms/:id/messages", async (req, res, next) => {
-        const { id } = req.params;
-        const messages = await Message.find({
-            room: new mongodb.ObjectId(id)
-        }).toArray();
-        res.json({
-            data: messages
-        })
+  app.use(express.static("../fe/public"));
+
+  app.post("/api/users/login", async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+
+    if (user) {
+      res.cookie("user", user._id);
+      return res.sendStatus(200);
+    }
+
+    return res.sendStatus(400);
+  });
+
+  app.get("/api/rooms", async (req, res) => {
+    const user = req.cookies["user"];
+
+    const rooms = await Room.find({
+      users: mongodb.ObjectId(user),
+    }).toArray();
+    return res.json(rooms);
+  });
+
+  app.get("/api/rooms/:id/messages", async (req, res, next) => {
+    const { id } = req.params;
+    const messages = await Message.find({
+      room: new mongodb.ObjectId(id),
+    }).toArray();
+    res.json({
+      data: messages,
+    });
+  });
+
+  app.post("/api/rooms/:id/messages", async (req, res, next) => {
+    const { id } = req.params;
+    await Message.insertOne({
+      room: new mongodb.ObjectId(id),
+      body: req.body.body,
     });
 
-    app.post("/api/rooms/:id/messages", async (req, res, next) => {
-        const { id } = req.params;
-        await Message.insertOne({
-            room: new mongodb.ObjectId(id),
-            body: req.body.body
-        });
+    io.emit("message", req.body.body);
 
-        io.emit("message", req.body.body);
+    res.sendStatus(200);
+  });
 
-        res.sendStatus(200);
-    });
-
-    server.listen(1337);
+  server.listen(1337);
 }
 
 run();
--- chat/fe/src/app.ts
+++ chat/fe/src/app.ts
@@ -1,70 +1,70 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
 const socket = io();
 
 socket.on("message", (body) => {
-    const newMessage = Message({ body });
-    messageList.appendChild(newMessage);
-    newMessage.scrollIntoView();
-})
+  const newMessage = Message({ body });
+  messageList.appendChild(newMessage);
+  newMessage.scrollIntoView();
+});
 
 function Message(props: { body: string }) {
-    const el = Div({
-        class: "message"
-    });
+  const el = Div({
+    class: "message",
+  });
 
-    el.innerText = props.body;
+  el.innerText = props.body;
 
-    return el;
+  return el;
 }
 
 function MessageList() {
-    const el = Div({
-        class: "message-list"
-    });
+  const el = Div({
+    class: "message-list",
+  });
 
-    fetch("/api/rooms/611f1cca1def03484db6db32/messages").then(async (res) => {
-        const jsonData = await res.json();
-        const messages = jsonData.data;
-        for (const message of messages) {
-            el.appendChild(Message({ body: message.body }));
-        }
-    })
+  fetch("/api/rooms/611f1cca1def03484db6db32/messages").then(async (res) => {
+    const jsonData = await res.json();
+    const messages = jsonData.data;
+    for (const message of messages) {
+      el.appendChild(Message({ body: message.body }));
+    }
+  });
 
-    return el;
+  return el;
 }
 
-function Div(attributes: { id?: string, class?: string } = {}) {
-    const div = document.createElement("div");
+function Div(attributes: { id?: string; class?: string } = {}) {
+  const div = document.createElement("div");
 
-    for (const attribute in attributes) {
-        div.setAttribute(attribute, attributes[attribute]);
-    }
+  for (const attribute in attributes) {
+    div.setAttribute(attribute, attributes[attribute]);
+  }
 
-    return div;
+  return div;
 }
 
 function Input() {
-    return document.createElement("input");
+  return document.createElement("input");
 }
 
-function TextBox(props: { onSubmit: (text: string) => void; }) {
-    const el = Div({
-        class: "textbox"
-    });
+function TextBox(props: { onSubmit: (text: string) => void }) {
+  const el = Div({
+    class: "textbox",
+  });
 
-    const input = Input();
+  const input = Input();
 
-    input.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            props.onSubmit(input.value);
-            input.value = "";
-        }
-    })
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      props.onSubmit(input.value);
+      input.value = "";
+    }
+  });
 
-    el.appendChild(input);
+  el.appendChild(input);
 
-    return el;
+  return el;
 }
 
 const root = document.getElementById("root");
@@ -72,18 +72,39 @@ const root = document.getElementById("root");
 const messageList = MessageList();
 
 function handleSubmit(text: string) {
-    fetch("/api/rooms/611f1cca1def03484db6db32/messages", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ body: text }),
-    }).catch((err) => {
-        alert(err.message);
-    });
+  fetch("/api/rooms/611f1cca1def03484db6db32/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ body: text }),
+  }).catch((err) => {
+    alert(err.message);
+  });
 }
 
 const textBox = TextBox({ onSubmit: handleSubmit });
 
 root.appendChild(messageList);
 root.appendChild(textBox);
+
+const btnLogin = document.getElementById("login");
+btnLogin.addEventListener("click", async function () {
+  const email = prompt("email");
+  const password = prompt("password");
+
+  const res = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  if (res.ok) {
+    btnLogin.style.display = "none";
+  }
+});
