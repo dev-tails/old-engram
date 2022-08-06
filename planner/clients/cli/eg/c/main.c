b342350f754788fd7c27ca9c4053fdb55382c84c
Adam Berg <adam@xyzdigital.com>
2022-08-06 09:32:11 -0700

    Default writing to home directory

--- planner/clients/cli/eg/c/main.c
+++ planner/clients/cli/eg/c/main.c
@@ -1,16 +1,63 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <stdio.h>
+#define clrscr() printf("\e[1;1H\e[2J")
+#endif
 #define MAX_LINE_SIZE 512
+
+const char* notes_path = "~/.engram/notes.txt";
 
 int main(int argc, char *argv[])
 {
-  FILE *fp = fopen("notes.txt", "a");
+  clrscr();
+
+  struct stat st = {0};
+
+  const char *home_dir = getenv("HOME");
+  char engram_dir[128];
+  
+  sprintf(engram_dir, "%s/%s", home_dir, ".engram");
+
+  if (stat(engram_dir, &st) == -1) {
+      mkdir(engram_dir, 0700);
+  }
+
+  int running = 1;
+  int consecutive_newlines = 0;
+
+  char notes_file_path[128];
+  sprintf(notes_file_path, "%s/%s", engram_dir, "notes.txt");
+
+  FILE *fp = fopen(notes_file_path, "a");
+  if (fp == NULL) {
+    return 1;
+  }
 
   char buffer[MAX_LINE_SIZE];
 
-  fgets(buffer, MAX_LINE_SIZE, stdin);
-  printf("%s", buffer);
-  fputs(buffer, fp);
+  do {
+    fgets(buffer, MAX_LINE_SIZE, stdin);
+
+    if (buffer[0] == '\n') {
+      if (consecutive_newlines > 0) {
+        running = 0;
+      } else {
+        consecutive_newlines++;
+        clrscr();
+      }
+    } else {
+      consecutive_newlines = 0;
+      fputs(buffer, fp);
+    }
+  } while(running);
 
   return 0;
 }
