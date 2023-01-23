87d124a9e2ebfa05a81e69b8aa471b19fe6d7a42
iyriss <iyrisvigil@gmail.com>
2023-01-23 13:40:02 -0800

    show video when joining

--- meet/src/views/Video.ts
+++ meet/src/views/Video.ts
@@ -1,14 +1,19 @@
-import { Button } from '../../../ui/components/Button';
 import { io } from 'socket.io-client';
 import Peer from 'peerjs';
+import { Div } from '../../../ui/components/Div';
 const socket = io();
 
+const getUserMedia =
+  (navigator as any).getUserMedia ||
+  (navigator as any).webkitGetUserMedia ||
+  (navigator as any).mozGetUserMedia;
+
 export function Video() {
-  const roomId = window.location.pathname;
+  const roomId = window.location.pathname.split('/')[1];
 
   socket.emit('join-room', 'ROOM_ID', roomId);
 
-  const myPeer = new Peer(roomId);
+  const myPeer = new Peer();
 
   myPeer.on('open', (id) => {
     // here we are sending an event to our server
@@ -19,9 +24,48 @@ export function Video() {
     console.log('User conncted ', userId);
   });
 
-  const el = Button({
-    innerText: 'Call',
+  const el = Div();
+
+  const myVideo = document.createElement('video');
+  myVideo.muted = true;
+  const peers = {};
+  getUserMedia({
+    video: true,
+    audio: true,
+  }).then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on('call', (call) => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
+
+  function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+      video.play();
+    });
+    el.append(video);
+  }
+
+  function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream); // call and send our video stream
+    const otherUserVideo = document.createElement('video');
+    call.on('stream', (userVideoStream) => {
+      addVideoStream(otherUserVideo, userVideoStream);
+    });
+    call.on('close', () => {
+      otherUserVideo.remove();
+    });
+  }
 
   return el;
 }
